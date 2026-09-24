6e040a9b266b3312b680e064bab23debf6d4cec9
IvTole <toledano.ivan16@gmail.com>
2026-09-24 08:00:06 -0600

    add config, workflow variables

---
+++ 07_pytorch_modular_intro/src/config.py
@@ -0,0 +1,15 @@
+# Variables utilizadas en el flujo de trabajo
+
+# Data paths
+
+TRAIN_PATH = "../data/train.csv"
+TEST_PATH = "../data/test.csv"
+
+# Random seed
+SEED = 42
+
+
+# TRAINING
+EPOCHS = 100
+LEARNING_RATE = 0.05
+HIDDEN_UNITS = 20
